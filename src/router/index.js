9cd5ff76b647d3e0866b8d80daede5df030b5833
David Truxall <david.truxall@perspecta.com>
2021-03-25 09:48:02 -0400

    Renamed items and changed content

--- src/router/index.js
+++ src/router/index.js
@@ -8,15 +8,21 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      isAuthenticated: false
+    }
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/secured',
+    name: 'Secured',
+    meta: {
+      isAuthenticated: true
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Secured.vue')
   }
 ]
 
